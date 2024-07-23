9bcc6b942b55a01652128ba0d2a78edc2c8edcfa
dist <113162128+dist94@users.noreply.github.com>
2024-07-23 21:47:53 +1000

    Create lib2.js

---
+++ lib2.js
@@ -0,0 +1,295 @@
+(function (_0x1ef5c5, _0x4e8aca) {
+  var _0x4dbcab = _0x1ef5c5();
+  while (true) {
+    try {
+      var _0x2ab03a = -parseInt(_0x24de(520, 0x17c)) / 1 + parseInt(_0x24de(433, 0x11f)) / 2 * (parseInt(_0x24de(430, 0x1b2)) / 3) + parseInt(_0x24de(373, 0x15a)) / 4 + parseInt(_0x24de(460, 0x104)) / 5 + parseInt(_0x24de(525, 0x21d)) / 6 + -parseInt(_0x24de(485, 0x191)) / 7 + -parseInt(_0x24de(537, 0x175)) / 8 * (parseInt(_0x24de(416, 0x158)) / 9);
+      if (_0x2ab03a === _0x4e8aca) {
+        break;
+      } else {
+        _0x4dbcab.push(_0x4dbcab.shift());
+      }
+    } catch (_0x4de933) {
+      _0x4dbcab.push(_0x4dbcab.shift());
+    }
+  }
+})(_0x5ec7, 955378);
+function _createForOfIteratorHelper(_0x277be2, _0x5154ce) {
+  var _0x23f93c;
+  var _0x23c254;
+  var _0x58f579;
+  var _0xda9320;
+  var _0x5519f1 = "undefined" != typeof Symbol && _0x277be2[Symbol.iterator] || _0x277be2["@@iterator"];
+  if (_0x5519f1) {
+    _0x23c254 = !(_0x23f93c = true);
+    return {
+      "s": function () {
+        _0x5519f1 = _0x5519f1.call(_0x277be2);
+      },
+      "n": function () {
+        var _0x254314 = _0x5519f1.next();
+        _0x23f93c = _0x254314.done;
+        return _0x254314;
+      },
+      "e": function (_0x176ef4) {
+        _0x23c254 = true;
+        _0x58f579 = _0x176ef4;
+      },
+      "f": function () {
+        try {
+          if (!(_0x23f93c || null == _0x5519f1["return"])) {
+            _0x5519f1["return"]();
+          }
+        } finally {
+          if (_0x23c254) {
+            throw _0x58f579;
+          }
+        }
+      }
+    };
+  }
+  if (Array.isArray(_0x277be2) || (_0x5519f1 = _unsupportedIterableToArray(_0x277be2)) || _0x5154ce && _0x277be2 && "number" == typeof _0x277be2.length) {
+    if (_0x5519f1) {
+      _0x277be2 = _0x5519f1;
+    }
+    _0xda9320 = 0;
+    return {
+      "s": _0x5154ce = function () {},
+      "n": function () {
+        var _0x17338c = {
+          done: true
+        };
+        return _0xda9320 >= _0x277be2.length ? _0x17338c : {
+          "done": false,
+          "value": _0x277be2[_0xda9320++]
+        };
+      },
+      "e": function (_0x192536) {
+        throw _0x192536;
+      },
+      "f": _0x5154ce
+    };
+  }
+  throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+function _toConsumableArray(_0x31a216) {
+  return _arrayWithoutHoles(_0x31a216) || _iterableToArray(_0x31a216) || _unsupportedIterableToArray(_0x31a216) || _nonIterableSpread();
+}
+function _0x24de(_0x1657d7, _0x1fe6a7) {
+  var _0x10764a = _0x5ec7();
+  _0x24de = function (_0x34ab58, _0x569dba) {
+    _0x34ab58 = _0x34ab58 - 310;
+    var _0x3da7d6 = _0x10764a[_0x34ab58];
+    return _0x3da7d6;
+  };
+  return _0x24de(_0x1657d7, _0x1fe6a7);
+}
+function _nonIterableSpread() {
+  throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+function _unsupportedIterableToArray(_0x11055b, _0x572534) {
+  var _0x530896;
+  if (_0x11055b) {
+    return "string" == typeof _0x11055b ? _arrayLikeToArray(_0x11055b, _0x572534) : "Map" === (_0x530896 = "Object" === (_0x530896 = Object.prototype.toString.call(_0x11055b).slice(8, -1)) && _0x11055b.constructor ? _0x11055b.constructor.name : _0x530896) || "Set" === _0x530896 ? Array.from(_0x11055b) : "Arguments" === _0x530896 || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(_0x530896) ? _arrayLikeToArray(_0x11055b, _0x572534) : undefined;
+  }
+}
+function _iterableToArray(_0x33978c) {
+  if ("undefined" != typeof Symbol && null != _0x33978c[Symbol.iterator] || null != _0x33978c["@@iterator"]) {
+    return Array.from(_0x33978c);
+  }
+}
+function _arrayWithoutHoles(_0x222cce) {
+  if (Array.isArray(_0x222cce)) {
+    return _arrayLikeToArray(_0x222cce);
+  }
+}
+function _arrayLikeToArray(_0x24ba37, _0xacf58a) {
+  if (null == _0xacf58a || _0xacf58a > _0x24ba37.length) {
+    _0xacf58a = _0x24ba37.length;
+  }
+  var _0x302b42 = 0;
+  for (var _0x2b091c = new Array(_0xacf58a); _0x302b42 < _0xacf58a; _0x302b42++) {
+    _0x2b091c[_0x302b42] = _0x24ba37[_0x302b42];
+  }
+  return _0x2b091c;
+}
+function asyncGeneratorStep(_0x146f37, _0x6f1018, _0x91892f, _0x34f8d6, _0x1e19f5, _0x3b6c6d, _0x316ea2) {
+  try {
+    var _0x25d7b9 = _0x146f37[_0x3b6c6d](_0x316ea2);
+    var _0x1cc3ce = _0x25d7b9.value;
+  } catch (_0x14e3a7) {
+    return void _0x91892f(_0x14e3a7);
+  }
+  if (_0x25d7b9.done) {
+    _0x6f1018(_0x1cc3ce);
+  } else {
+    Promise.resolve(_0x1cc3ce).then(_0x34f8d6, _0x1e19f5);
+  }
+}
+function _asyncToGenerator(_0x2ccd71) {
+  return function () {
+    var _0x227312 = this;
+    return new Promise(function (_0x403748, _0x2d42fb) {
+      var _0x450f84 = _0x2ccd71.apply(_0x227312, arguments);
+      function _0x3cffa5(_0x16fcd2) {
+        asyncGeneratorStep(_0x450f84, _0x403748, _0x2d42fb, _0x3cffa5, _0x5c7df9, "next", _0x16fcd2);
+      }
+      function _0x5c7df9(_0x3f371f) {
+        asyncGeneratorStep(_0x450f84, _0x403748, _0x2d42fb, _0x3cffa5, _0x5c7df9, "throw", _0x3f371f);
+      }
+      _0x3cffa5(undefined);
+    });
+  };
+}
+function _0x5109a8(_0x3928cc, _0x3ca156) {
+  return _0x24de(_0x3ca156 - 0x9a, _0x3928cc);
+}
+function _0x5ec7() {
+  var _0x5770f5 = ["bJJlU", "ysNoc", "$& ", "return", "ray object", "gpesk", "WtNvP", "Gota.io - ", "qHupS", "pCjpl", "Header", " from <a h", "ninKC", "GmaMy", "GDgvc", "XyXKd", "call", "JHfFd", "blruU", "PNRCx", "https://un", "FBDbp", "yOkYx", "Jjpcb", "prev", "kFHah", "XMLHttpReq", "onload", "number", "-iterable ", "GM_info", "Map", "JAAvk", "BHTHy", "string", "KCKFM", "n order to", ".dropboxus", "y Camlan", "open", "isArray", "ol.iterato", "</a> and t", "sPvRd", "JZjrY", "Gota Fixes", "match", "emoKH", "ZUZgn", "Promise", "name", " | ", "setRequest", "ercontent.", "jcWnj", "qjTjb", "nSKzO", "from", "replace", "MeVrH", "LUBOE", "setAttribu", "JeZEk", "avkxe", "FPbUp", "@@iterator", " (CTRL + R", "location", "retrieve l", "terate non", "rlkey=xamm", "done", "xi.js-lega", "Event", "Object", "YeMfs", "dBXNl", "downloadUR", "https://dl", "iterator", "DMrhv", "GfGPW", "Client ver", "atest scri", "xVRwN", "pkg.com/pi", "gajsn", "r]() metho", "toString", "apply", "Vaiuc", "eld", "ts must ha", "status", "ble, non-a", "readystate", "rray objec", "6295280TwbQBM", "KUXhY", "script", "throw", "Failed to ", "async", "iterable i", "bxCof", "aYflf", "ngXvI", "then", "giOLG", "h the page", "kQyTt", "title", "ase instal", "dispatchEv", " date. Ple", "ref=\"", "replaceWit", "cy@7.2.4/d", "UKfvz", "constructo", "instance.\n", "tmaXX", "xIubj", "range", "fetch ", "Set", "change", "pOGeE", "delegateYi", ". Code: ", "XDdzu", "jLyfN", "WEFvv", "/su0my3/s?", "onerror", "length", "MzaIP", "gZTbE", "PttSZ", "assign", "9AaqAFa", "s must hav", "slice", "MpmXj", "Error", "Xsqtg", "DOMContent", "body", "sByClassNa", "HXqtH", "pre-line", "version", "tzYlZ", "Arguments", "171TytzOK", "Yjxon", "innerHTML", "35386Hlaxxg", "EsJIl", "mark", "KtnhW", "heqzX", "attributes", "UbWfQ", "ecwpi", "pRcii", "sion:", "ipnsy", "IcLon", "UXTZD", "jLrVk", "o be itera", "gclEY", "style", "undefined", "src", "value", "bytes=27-2", "createElem", "le, non-ar", "gklNc", "gota.js", " is out of", "responseTe", "6579625FAprOT", "prototype", "ist/pixi-l", "Invalid at", "ooju6ecn15", "HlVoF", "l the late", "gpvHZ", "ve a [Symb", "sent", " be iterab", "19cbsggfcx", "Improved b", "GPJLk", "eval", "nsjag", "ZeuOi", "QbOKI", "]() method", "WcQNb", "uJkvY", "RiXHx", "st version", "corQF", "ement", "8295525eGziwV", "e a [Symbo", "EzDAV", "end", "Loaded", "nstance.\nI", "getElement", "documentEl", "ciKPB", "fvrNv", "RizpW", "tempt to i", "pt version", "In order t", "gdxTH", "obHeR", "IHuoE", "includes", "SMNCI", "mKsTK", "IJocJ", "main-versi", "QSiRn", "uest", "UVjCZ", "xtwqx", "send", "CZlFR", "hen refres", "xPFrP", "egacy.min.", "hZiCB", "EIZuc", "load", "FQPYB", "1821292VPZjsF", "wrap", "qGONa", "test", "GET", "3782316qpKTDC", "IECss", "EIvnv", "l.iterator", "center", "Amghv", "sqXYu", "innerText", "tempt to s", "OcpSQ", "jigbX", "NRBCT", "4535096BXLcPf", "hCppi", "href", "next", "stop", "18px", "pread non-", "scripts", "ent", "com/scl/fi", "resolve", "QLwho"];
+  _0x5ec7 = function () {
+    return _0x5770f5;
+  };
+  return _0x5ec7();
+}
+_asyncToGenerator(regeneratorRuntime.mark(function e(_0x2e5fcd, _0x45d7b6) {
+  var _0x34578b;
+  var _0x2530e5;
+  var _0xfcecef;
+  var _0x1bbde5;
+  var _0x5567c3;
+  var _0x4f5b3b;
+  var _0x439ff4;
+  var _0x3bc194;
+  var _0x4919ae;
+  var _0x1d3c69;
+  return regeneratorRuntime.wrap(function (_0xfd97ee) {
+    for (;;) {
+      switch (_0xfd97ee.prev = _0xfd97ee.next) {
+        case 0:
+          var _0xe75391 = {
+            range: "bytes=27-282"
+          };
+          _0x34578b = function (_0x5399f7) {
+            throw new _0x2e5fcd.Error(_0x5399f7);
+          };
+          _0x1bbde5 = function (_0x441890, _0x33fd75) {
+            if (undefined === _0x33fd75) {
+              _0x33fd75 = {};
+            }
+            var _0x1d9fa9;
+            var _0x42948d;
+            var _0x5c0cb0 = new _0x2e5fcd.XMLHttpRequest();
+            _0x5c0cb0.open("GET", _0x441890, false);
+            for (_0x1d9fa9 in _0x33fd75) _0x5c0cb0.setRequestHeader(_0x1d9fa9, _0x33fd75[_0x1d9fa9]);
+            _0x5c0cb0.send();
+            if (200 <= (_0x42948d = _0x5c0cb0.status) && _0x42948d < 300) {
+              return _0x5c0cb0.responseText;
+            }
+            _0x34578b("Failed to fetch " + _0x441890 + ". Code: " + _0x5c0cb0.status);
+          };
+          _0x4f5b3b = _0x2e5fcd.GM_info.script;
+          _0x5567c3 = _0x4f5b3b.version;
+          _0x4f5b3b = _0x4f5b3b.downloadURL;
+          delete _0x2e5fcd.GM_info;
+          if (!(_0x439ff4 = _0x1bbde5(_0x4f5b3b, _0xe75391).match(/@version\s+([\d.]+)/))) {
+            _0x34578b("Failed to retrieve latest script version.");
+          }
+          if (_0x5567c3 === _0x439ff4[1]) {
+            _0xfd97ee.next = 6;
+          } else {
+            _0x45d7b6.documentElement.innerHTML = "Gota Fixes is out of date. Please install the latest version from <a href=\"" + _0x4f5b3b + "\">" + _0x4f5b3b + "</a> and then refresh the page (CTRL + R)!";
+            _0x2e5fcd.Object.assign(_0x45d7b6.body.style, {
+              "textAlign": "center",
+              "fontSize": "18px",
+              "whiteSpace": "pre-line"
+            });
+            _0xfd97ee.next = 25;
+          }
+          break;
+        case 6:
+          _0x45d7b6.documentElement.innerHTML = _0x1bbde5(_0x2e5fcd.location.href);
+          _0x3bc194 = 0;
+          _0x4919ae = _arrayWithoutHoles(_0x45d7b6.scripts) || _iterableToArray(_0x45d7b6.scripts) || _unsupportedIterableToArray(_0x45d7b6.scripts) || _nonIterableSpread();
+        case 8:
+          if (_0x3bc194 < _0x4919ae.length) {
+            if ((_0x1d3c69 = _0x4919ae[_0x3bc194]).src.includes("gota.js")) {
+              _0xfd97ee.t0 = _0x2e5fcd;
+              _0xfd97ee.next = 14;
+              return _0x2e5fcd.nsjag("https://dl.dropboxusercontent.com/scl/fi/su0my3/s?rlkey=xammooju6ecn1519cbsggfcx1");
+            }
+            _0xfd97ee.next = 18;
+          } else {
+            _0xfd97ee.next = 22;
+          }
+          break;
+        case 14:
+          _0xfd97ee.t1 = _0xfd97ee.sent;
+          _0xfd97ee.t0.eval.call(_0xfd97ee.t0, _0xfd97ee.t1);
+          _0xfd97ee.next = 19;
+          break;
+        case 18:
+          return _0xfd97ee.delegateYield(regeneratorRuntime.mark(function _0x5783bc() {
+            var _0x33a981;
+            var _0x7b46b2;
+            var _0x5b2d27;
+            var _0x54ecae;
+            return regeneratorRuntime.wrap(function (_0x35d6af) {
+              for (;;) {
+                switch (_0x35d6af.prev = _0x35d6af.next) {
+                  case 0:
+                    if (_0x1d3c69.src.match(/pixi[\w.-]+?js\?v=([\d.]+)/)) {
+                      _0x1d3c69.setAttribute("src", "https://unpkg.com/pixi.js-legacy@7.2.4/dist/pixi-legacy.min.js");
+                    }
+                    (_0x33a981 = _0x45d7b6.createElement("script")).innerHTML = _0x1d3c69.innerHTML;
+                    _0x7b46b2 = _createForOfIteratorHelper(_0x1d3c69.attributes);
+                    try {
+                      for (_0x7b46b2.s(); !(_0x5b2d27 = _0x7b46b2.n()).done;) {
+                        _0x54ecae = _0x5b2d27.value;
+                        _0x2530e5 = _0x54ecae.name;
+                        _0xfcecef = _0x54ecae.value;
+                        _0x33a981.setAttribute(_0x2530e5, _0xfcecef);
+                      }
+                    } catch (_0x3705a7) {
+                      _0x7b46b2.e(_0x3705a7);
+                    } finally {
+                      _0x7b46b2.f();
+                    }
+                    _0x1d3c69.replaceWith(_0x33a981);
+                    _0x35d6af.t0 = _0x1d3c69.src && !_0x1d3c69.async;
+                    if (_0x35d6af.t0) {
+                      _0x35d6af.next = 10;
+                      return new _0x2e5fcd.Promise(function (_0x2a9f3c) {
+                        return _0x33a981.onload = _0x33a981.onerror = _0x2a9f3c;
+                      });
+                    }
+                    _0x35d6af.next = 10;
+                    break;
+                  case 10:
+                  case "end":
+                    return _0x35d6af.stop();
+                }
+              }
+            }, _0x5783bc);
+          })(), "t2", 19);
+        case 19:
+          _0x3bc194++;
+          _0xfd97ee.next = 8;
+          break;
+        case 22:
+          _0x2e5fcd.gajsn();
+          delete _0x2e5fcd.gajsn;
+          _0x5567c3 = _0x45d7b6.getElementsByClassName("main-version")[0];
+          _0x439ff4 = "Improved by Camlan";
+          _0x5567c3.innerText = _0x5567c3.innerText.replace("Client version:", "$& " + _0x2e5fcd.version + " | " + _0x439ff4);
+          _0x45d7b6.title = "Gota.io - " + _0x439ff4;
+          _0x4f5b3b = new _0x2e5fcd.Event("DOMContentLoaded");
+          _0x45d7b6.dispatchEvent(_0x4f5b3b);
+          _0x2e5fcd.dispatchEvent(_0x4f5b3b);
+          _0x45d7b6.dispatchEvent(new _0x2e5fcd.Event("readystatechange"));
+          _0x2e5fcd.dispatchEvent(new _0x2e5fcd.Event("load"));
+        case 25:
+        case "end":
+          return _0xfd97ee.stop();
+      }
+    }
+  }, e);
+}))(window, document);
